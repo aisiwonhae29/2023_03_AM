982acda2fa81ad839e5efc33ecd7fae5a009cfae
aisiwonhae29 <aisiwonhae29@gmail.com>
2023-03-08 17:43:38 +0900

    입력 변경 완료

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,25 +1,82 @@
 package com.KoreaIT.java.AM;
 
-
-
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
+		int 순서 = 1;
 		
-		Scanner sc = new Scanner(System.in);
+		ArrayList a = new ArrayList();  
 		
-		String command = sc.nextLine();
-		System.out.printf("입력된 명령어 : %s\n", command);
-		
-		int command2 = sc.nextInt();
-		System.out.printf("입력된 명령어 : %d\n", command2);
-		
+		while (true) {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("명령어 > ");
+			String command = sc.nextLine();
+
+			if (command.equals("article write")) {
+				a.add(new board());
+				board A =(board) a.get(순서-1);
+				
+				System.out.printf("제목 > ");
+				String 제목 = sc.nextLine();
+				System.out.printf("내용 > ");
+				String 내용 = sc.nextLine();
+				System.out.println();
+				
+				A.제목 = 제목;
+				A.내용 = 내용;
+
+				System.out.print("제목 : ");
+				System.out.println(제목);
+				System.out.print("내용 : ");
+				System.out.println(내용);
+				System.out.printf("%d번째 글이 생성되었습니다\n", 순서);
+				순서++;
+
+			} else if (command.equals("exit")) {
+				break;
+
+			}else if(command.equals("read")){
+				int num = sc.nextInt();
+				
+				board A_1=(board) a.get(num-1);
+				System.out.println(A_1.제목+"\n"+A_1.내용);
+			
+			}else if (command.equals("article list")) {
+				if (순서 == 1) {
+					System.out.println("게시글이 없습니다");
+				} else {
+					System.out.printf("게시글이 %d개 있습니다\n", 순서 - 1);
+				}
+
+			} else if(command.equals("list")){
+				for (int i=0; i<순서-1; i++) {
+					board A_1=(board)a.get(i);
+					System.out.println(A_1.제목);
+				}
+				
+			}else if(command.equals("help")){
+				System.out.println("article write");
+				System.out.println("read");
+				System.out.println("list");
+				System.out.println("article list");
+				System.out.println("exit");
+			}else {
+				System.out.println("존재하지 않는 명령어 입니다");
+			}
+
+		}
+
 		System.out.println("==프로그램 끝==");
-		
-		sc.close();
+
 	}
 
+}
+
+class board{
+	String 제목;
+	String 내용;
+	int 순서;
 }
---
+++ src/com/KoreaIT/java/AM/asdf.java
@@ -0,0 +1,15 @@
+package com.KoreaIT.java.AM;
+
+import java.util.ArrayList;
+
+public class asdf {
+
+	public static void main(String[] args) {
+		ArrayList a = new ArrayList();
+		
+		a.add(1);
+		System.out.println(a.get(0));
+		a.get(0);
+	}
+
+}
